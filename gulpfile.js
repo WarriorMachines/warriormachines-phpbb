be3aac36283d562d94f358e81de722e1854f56b7
AustinMaddox <austin@maddoxbox.com>
2016-02-18 15:53:23 -0700

    Use gulp to generate one app.css, and separate 'fonts' task

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('default', ['sass'], function () {
-    //gulp.src('./node_modules/font-awesome/fonts/**/*.{eot,svg,ttf,woff,woff2}')
-    //    .pipe(gulp.dest('./styles/warriormachines_2016/theme/gulp-generated/fonts'));
+gulp.task('default', ['sass', 'fonts'], function () {
+
 });
 
 gulp.task('sass', function () {
-    return gulp.src('./styles/warriormachines_2016/theme/sass/*.scss')
+    return gulp.src('./styles/warriormachines_2016/theme/sass/app.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./styles/warriormachines_2016/theme/gulp-generated/css'));
 });
 
+gulp.task('fonts', function() {
+    gulp.src('./node_modules/font-awesome/fonts/**/*.{eot,svg,ttf,woff,woff2}')
+        .pipe(gulp.dest('./styles/warriormachines_2016/theme/gulp-generated/fonts'));
+});
+
 gulp.task('sass:watch', function () {
-    gulp.watch('./styles/warriormachines_2016/theme/sass/*.scss', ['sass']);
+    gulp.watch('./styles/warriormachines_2016/theme/sass/app.scss', ['sass']);
 });
