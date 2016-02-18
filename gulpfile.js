147aee1c9de8c1734a945cfe0ff2746f79293d8e
AustinMaddox <austin@maddoxbox.com>
2016-02-18 15:11:43 -0700

    Set up gulp to generate sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function () {
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('default', ['sass'], function () {
     //gulp.src('./node_modules/font-awesome/fonts/**/*.{eot,svg,ttf,woff,woff2}')
     //    .pipe(gulp.dest('./styles/warriormachines_2016/theme/gulp-generated/fonts'));
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./styles/warriormachines_2016/theme/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./styles/warriormachines_2016/theme/gulp-generated/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./styles/warriormachines_2016/theme/sass/*.scss', ['sass']);
 });
